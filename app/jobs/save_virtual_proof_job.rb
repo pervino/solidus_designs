730f5dde058372245b16d6f17c86d81ab3e9ae3e
Anthony D'Addeo <amdaddeo@gmail.com>
2016-07-29 14:40:45 -0500

    Simplify save job

--- app/jobs/save_virtual_proof_job.rb
+++ app/jobs/save_virtual_proof_job.rb
@@ -2,7 +2,7 @@ class SaveVirtualProofJob < ActiveJob::Base
   queue_as :default
 
   def perform(customization)
-    customization.virtual_proof = URI.parse(customization.virtual_proof_url)
+    customization.virtual_proof = customization.virtual_proof_url
     customization.save
   end
 end
--- solidus_designs.gemspec
+++ solidus_designs.gemspec
@@ -12,7 +12,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'solidus_designs'
-  s.version     = '0.1.31'
+  s.version     = '0.1.32'
   s.required_ruby_version = '>= 2.1'
 
   s.author    = ['Anthony Daddeo']
