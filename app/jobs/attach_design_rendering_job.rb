b234c98b25ee7a343df9b83473305fc1fbb2c99b
Anthony D'Addeo <amdaddeo@gmail.com>
2016-10-06 12:07:57 -0500

    Use URI.parse

--- app/jobs/attach_design_rendering_job.rb
+++ app/jobs/attach_design_rendering_job.rb
@@ -2,7 +2,7 @@ class AttachDesignRenderingJob < ActiveJob::Base
   queue_as :default
 
   def perform(design)
-    design.rendering = design.render_url
+    design.rendering = URI.parse(design.render_url)
     design.save
   end
 end
