2550b177fd52901eac47ec2ce2920193e213ea2f
Anthony D'Addeo <amdaddeo@gmail.com>
2016-10-31 15:26:00 -0500

    Change default design_options scope

--- app/models/spree/design_configuration.rb
+++ app/models/spree/design_configuration.rb
@@ -7,7 +7,7 @@ module Spree
     store_accessor :meta, :description, :dimensions
 
     belongs_to :product, touch: true
-    has_many :design_options, -> { with_deleted.order(position: :asc) }, dependent: :destroy
+    has_many :design_options, -> { order(position: :asc) }, dependent: :destroy
 
     validates :name, presence: true
     validates :size, presence: true
