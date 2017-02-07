d5cf7a20e4913d97b3faf93779fb84d60dd829de
Aaron Murray <akmurray@gmail.com>
2017-02-07 12:14:45 -0600

    allow template save for popularity field

--- lib/spree/permitted_attributes_decorator.rb
+++ lib/spree/permitted_attributes_decorator.rb
@@ -6,6 +6,6 @@ Spree::PermittedAttributes.module_eval do
   ])
 
   class_variable_set(:@@template_attributes, [
-      :name, :medium, :display, :tag_list
+      :name, :medium, :display, :tag_list, :popularity
   ])
 end
