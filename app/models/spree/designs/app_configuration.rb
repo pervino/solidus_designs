bcd02f254222591cfad4b29294f9b558c9d62bfa
keithbooher <keith.booher@yahoo.com>
2019-10-31 14:45:10 -0500

    veuve typo

--- app/models/spree/designs/app_configuration.rb
+++ app/models/spree/designs/app_configuration.rb
@@ -2,7 +2,7 @@ module Spree
   class Designs::AppConfiguration < Preferences::Configuration
     preference :tags, :array, default: [:holidays, :occasions, :recipients, :colors]
 
-    preference :sizes, :array, default: [:bordeaux, :square, :teardrop, :woodbox_single, :woodbox_double, :woodbox_triple, :vueve]
+    preference :sizes, :array, default: [:bordeaux, :square, :teardrop, :woodbox_single, :woodbox_double, :woodbox_triple, :veuve]
 
     preference :mediums, :array, default: [:label, :engraving]
   end
