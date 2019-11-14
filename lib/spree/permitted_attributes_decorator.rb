a155216244b5679a1872c805d6c5be4a228b1a5a
keithbooher <keith.booher@yahoo.com>
2019-11-14 13:20:37 -0600

    fill color added to permitted attributes

--- lib/spree/permitted_attributes_decorator.rb
+++ lib/spree/permitted_attributes_decorator.rb
@@ -2,7 +2,7 @@ Spree::PermittedAttributes.module_eval do
   mattr_accessor :design_attributes, :template_attributes, :template_design_attributes
 
   class_variable_set(:@@design_attributes, [
-      :user_id, :medium, :name, :size, :markup, :render_url, :source_id, :template_id
+      :user_id, :medium, :name, :size, :markup, :render_url, :source_id, :template_id, :engraving_fill_color
   ])
 
   class_variable_set(:@@template_attributes, [
