b6538054e4551907980e028747e9650f185b22bf
keithbooher <keith.booher@yahoo.com>
2020-01-31 18:32:06 -0600

    comma not period

--- app/models/spree/designs/app_configuration.rb
+++ app/models/spree/designs/app_configuration.rb
@@ -2,7 +2,7 @@ module Spree
   class Designs::AppConfiguration < Preferences::Configuration
     preference :tags, :array, default: [:holidays, :occasions, :recipients, :colors]
 
-    preference :sizes, :array, default: [:bordeaux, :square, :teardrop, :woodbox_single, :woodbox_double, :woodbox_triple, :woodbox_photo_single, :woodbox_photo_double, :woodbox_photo_triple, :veuve. :custom_drink]
+    preference :sizes, :array, default: [:bordeaux, :square, :teardrop, :woodbox_single, :woodbox_double, :woodbox_triple, :woodbox_photo_single, :woodbox_photo_double, :woodbox_photo_triple, :veuve, :custom_drink]
 
     preference :mediums, :array, default: [:label, :engraving]
   end
