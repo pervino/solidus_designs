3360bb34933338ffb294e8f2221562fa739e0a83
keithbooher <keith.booher@yahoo.com>
2020-05-29 12:27:26 -0500

    permitted att for design option id in designs

--- lib/spree/permitted_attributes_decorator.rb
+++ lib/spree/permitted_attributes_decorator.rb
@@ -2,7 +2,7 @@ Spree::PermittedAttributes.module_eval do
   mattr_accessor :design_attributes, :template_attributes, :template_design_attributes
 
   class_variable_set(:@@design_attributes, [
-      :user_id, :medium, :name, :size, :markup, :render_url, :source_id, :template_id
+      :user_id, :medium, :name, :size, :markup, :render_url, :source_id, :template_id, :spree_design_options_id
   ])
 
   class_variable_set(:@@template_attributes, [
