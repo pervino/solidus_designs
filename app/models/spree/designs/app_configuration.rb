5813694b5f5c4c224c8445959cdcd1d35d33c4cb
keithbooher <keith.booher@yahoo.com>
2020-06-01 15:14:31 -0500

    simple mediums:

--- app/models/spree/designs/app_configuration.rb
+++ app/models/spree/designs/app_configuration.rb
@@ -4,6 +4,6 @@ module Spree
 
     preference :sizes, :array, default: [:bordeaux, :square, :teardrop, :woodbox_single, :woodbox_double, :woodbox_triple, :woodbox_magnum, :woodbox_photo_single, :woodbox_photo_double, :woodbox_photo_triple, :woodbox_photo_magnum, :veuve, :custom_drink]
 
-    preference :mediums, :array, default: [:label, :engraving]
+    preference :mediums, :array, default: [:label, :engraving, :simple_text, :simple_icon]
   end
 end
