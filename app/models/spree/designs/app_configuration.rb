71c4b79877314ad05b286d751a3d6bbf0c346a88
keithbooher <keith.booher@yahoo.com>
2020-08-05 12:28:33 -0500

    add new size

--- app/models/spree/designs/app_configuration.rb
+++ app/models/spree/designs/app_configuration.rb
@@ -2,7 +2,7 @@ module Spree
   class Designs::AppConfiguration < Preferences::Configuration
     preference :tags, :array, default: [:holidays, :occasions, :recipients, :colors]
 
-    preference :sizes, :array, default: [:bordeaux, :square, :teardrop, :woodbox_single, :woodbox_double, :woodbox_triple, :woodbox_magnum, :woodbox_photo_single, :woodbox_photo_double, :woodbox_photo_triple, :woodbox_photo_magnum, :veuve, :custom_drink]
+    preference :sizes, :array, default: [:bordeaux, :square, :teardrop, :woodbox_single, :woodbox_double, :woodbox_triple, :woodbox_magnum, :woodbox_photo_single, :woodbox_photo_double, :woodbox_photo_triple, :woodbox_photo_magnum, :veuve, :custom_drink, :mini_square]
 
     preference :mediums, :array, default: [:label, :engraving]
   end
